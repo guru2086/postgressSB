1ae040bca20c9cdfc56ee6b5649cd98ad364e5c7
guru2086 <guru2086@users.noreply.github.com>
2024-04-01 20:46:29 -0500

    created springboot project

---
+++ postgress_blucore/src/main/java/com/blucore/postgress_blucore/PostgressBlucoreApplication.java
@@ -0,0 +1,13 @@
+package com.blucore.postgress_blucore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgressBlucoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgressBlucoreApplication.class, args);
+	}
+
+}
---
+++ postgress_blucore/src/test/java/com/blucore/postgress_blucore/PostgressBlucoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blucore.postgress_blucore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgressBlucoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
